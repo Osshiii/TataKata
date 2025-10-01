f9803dd1766504af216ab1187b237cade4862a05
Putriani Pirma A. Sagala <144474403+PutrianiiPas@users.noreply.github.com>
2025-10-01 23:34:44 +0700

    Update checker.py

--- FastAPI/app/checker.py
+++ FastAPI/app/checker.py
@@ -4,12 +4,12 @@ def check_rules(text: str):
     errors = []
 
     if not re.match(r'^[A-Z]', text.strip()):
-        errors.append("Kalimat harus dimulai dengan huruf kapital")
+        errors.append("Kalimat harus dimulai dengan huruf kapital.")
 
     if re.search(r'\bdi[A-Za-z]', text):
-        errors.append("Gunakan spasi setelah kata depan 'di'")
+        errors.append("Gunakan spasi setelah kata depan 'di'.")
 
     if not text.strip().endswith(('.', '!', '?')):
-        errors.append("Kalimat harus diakhiri tanda baca (., !, ?)")
+        errors.append("Kalimat harus diakhiri tanda baca (., !, ?).")
 
     return errors
