5aef0f8134b0b6cfcd08689bd017b22cfa434509
Rosidah Darman <rosidahdarman01@gmail.com>
2025-10-21 13:58:36 +0700

    update

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+cd laravel_db
+
+composer install
+
+cp -n .env.example .env || true
+php artisan key:generate
+php artisan storage:link
+php artisan migrate --force
+
+php artisan serve --host=0.0.0.0 --port=${PORT}
