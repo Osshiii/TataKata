7575fe125575d27aaec283099e1749b9e7eca43c
Rosidah Darman <rosidahdarman01@gmail.com>
2025-10-21 16:01:08 +0700

    update

--- start.sh
+++ start.sh
@@ -18,5 +18,5 @@ php artisan key:generate
 php artisan migrate --force
 php artisan storage:link
 
-# Serve Laravel (backend + Blade/Vite frontend)
+# Serve Laravel di port Railway (gunakan PHP built-in server)
 php -S 0.0.0.0:${PORT} -t public
