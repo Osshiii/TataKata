17ff2c0280431fe59798bdf7d0d7be9b44b56b27
Rosidah Darman <rosidahdarman01@gmail.com>
2025-10-21 16:09:07 +0700

    up

--- start.sh
+++ start.sh
@@ -3,20 +3,16 @@ set -e
 
 cd laravel_db
 
-# Install backend & frontend dependencies
 composer install
 npm install
 npm run build
 
-# Setup SQLite (atau bisa diganti MySQL kalau pakai plugin)
 mkdir -p database
 touch database/database.sqlite
 
-# Setup Laravel
 cp -n .env.example .env || true
 php artisan key:generate
 php artisan migrate --force
 php artisan storage:link
 
-# Serve Laravel (backend + Blade/Vite frontend)
 php -S 0.0.0.0:${PORT} -t public
